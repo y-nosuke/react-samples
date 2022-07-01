9c36cbaae2c1864290516d4448a91440ab7bb433
nakao yoichi <yoichi-nakao@mamezou.com>
2022-07-01 16:51:50 +0900

    Index Routes

--- react-router-typescript/src/index.tsx
+++ react-router-typescript/src/index.tsx
@@ -18,6 +18,14 @@ root.render(
         <Route path="/" element={<App />}>
           <Route path="expenses" element={<Expenses />} />
           <Route path="invoices" element={<Invoices />}>
+            <Route
+              index
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p>Select an invoice</p>
+                </main>
+              }
+            />
             <Route path=":invoiceId" element={<Invoice />} />
           </Route>
           <Route
