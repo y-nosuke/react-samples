a80c3fa612f81327fccfbdb617355b71b6b66a06
yoichi <yoichi@physicist00.jp>
2022-07-03 06:14:41 +0900

    アップロードボタンのカスタマイズ

--- react-file-upload-typescript/src/App.tsx
+++ react-file-upload-typescript/src/App.tsx
@@ -1,10 +1,27 @@
+import { useRef } from "react";
+
 function App() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const onFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log(e.target.files);
   };
+
+  const fileUpload = () => {
+    console.log(inputRef.current);
+    inputRef.current?.click();
+  };
+
   return (
     <div>
-      <input type="file" onChange={onFileInputChange} multiple />
+      <button onClick={fileUpload}>ファイルアップロード</button>
+      <input
+        hidden
+        ref={inputRef}
+        type="file"
+        onChange={onFileInputChange}
+        multiple
+      />
     </div>
   );
 }
