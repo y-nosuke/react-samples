1f839b21118817c13e5f8ec156ba73925f1beee6
yoichi <yoichi@physicist00.jp>
2022-07-03 13:09:45 +0900

    ファイル単体をドロップ、ファイルオブジェクトを取得してみる

--- react-file-upload-typescript/src/DropDirectory.tsx
+++ react-file-upload-typescript/src/DropDirectory.tsx
@@ -2,7 +2,17 @@ import { DragEvent } from "react";
 import { DropArea } from "./DropArea";
 
 function DropDirectory() {
-  const handleDrop = (e: DragEvent<HTMLDivElement>) => {};
+  const handleDrop = async (e: DragEvent<HTMLDivElement>) => {
+    const item = e.dataTransfer.items[0];
+    const entry = item.webkitGetAsEntry();
+
+    const file = entry?.name;
+    if (entry?.isFile) {
+      console.log("これはファイルです。", file);
+    } else if (entry?.isDirectory) {
+      console.log("これはディレクトリです。", file);
+    }
+  };
 
   return (
     <DropArea onDrop={handleDrop}>
