0257745c27a472e541f8a8f502e165aac245ceec
yoichi <yoichi@physicist00.jp>
2022-07-03 14:09:35 +0900

    ディレクトリをドロップ、全てのファイルを取得してみる

--- react-file-upload-typescript/src/DropDirectory.tsx
+++ react-file-upload-typescript/src/DropDirectory.tsx
@@ -5,13 +5,28 @@ function DropDirectory() {
   const handleDrop = async (e: DragEvent<HTMLDivElement>) => {
     const item = e.dataTransfer.items[0];
     const entry = item.webkitGetAsEntry();
+    if (!entry) return;
 
-    const file = entry?.name;
-    if (entry?.isFile) {
-      console.log("これはファイルです。", file);
-    } else if (entry?.isDirectory) {
-      console.log("これはディレクトリです。", file);
-    }
+    const fileList: any = [];
+
+    const traverseFileTree = (entry: FileSystemEntry) => {
+      if (entry.isFile) {
+        fileList.push({ file: entry, path: entry.fullPath });
+      } else if (entry.isDirectory) {
+        const directoryReader = (
+          entry as FileSystemDirectoryEntry
+        ).createReader();
+        directoryReader.readEntries((entries) => {
+          entries.forEach((entry) => {
+            traverseFileTree(entry);
+          });
+        });
+      }
+    };
+
+    traverseFileTree(entry);
+
+    console.log(fileList);
   };
 
   return (
