76e7c3cd9b3f2f8568fdbdbbf615482883597147
yoichi <yoichi@physicist00.jp>
2022-07-08 00:31:53 +0900

    JSXで構造を作成

--- react-todo-typescript/src/App.tsx
+++ react-todo-typescript/src/App.tsx
@@ -1,5 +1,36 @@
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <div>
+            <li>あああ</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div>
+            <li>いいい</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTODO</p>
+        <ul>
+          <div>
+            <li>ううう</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default App;
