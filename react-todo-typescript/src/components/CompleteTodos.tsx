ea19ece99f736e3e298caf0902fcacf06b385f37
yoichi <yoichi@physicist00.jp>
2022-07-09 23:31:50 +0900

    カイゼン（コンポーネント化）

---
+++ react-todo-typescript/src/components/CompleteTodos.tsx
@@ -0,0 +1,18 @@
+export const CompleteTodos = (props: any) => {
+  const { todos, onClickBack } = props;
+  return (
+    <div className="complete-area">
+      <p className="title">完了のTODO</p>
+      <ul>
+        {todos.map((todo: any, index: number) => {
+          return (
+            <div key={todo} className="list-row">
+              <li>{todo}</li>
+              <button onClick={() => onClickBack(index)}>戻す</button>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
