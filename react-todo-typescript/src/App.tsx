d48195ef102f02ca14f3f5620c82af2eec9adc9a
yoichi <yoichi@physicist00.jp>
2022-07-09 23:51:27 +0900

    カイゼン（TODOの上限設定）

--- react-todo-typescript/src/App.tsx
+++ react-todo-typescript/src/App.tsx
@@ -51,7 +51,13 @@ function App() {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       ></InputTodo>
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>
+          登録できるtodo5個までだよ～。消化しろ～。
+        </p>
+      )}
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
--- react-todo-typescript/src/components/InputTodo.tsx
+++ react-todo-typescript/src/components/InputTodo.tsx
@@ -8,11 +8,18 @@ const style = {
 };
 
 export const InputTodo = (props: any) => {
-  const { todoText, onChange, onClick } = props;
+  const { todoText, onChange, onClick, disabled } = props;
   return (
     <div style={style}>
-      <input placeholder="TODOを入力" value={todoText} onChange={onChange} />
-      <button onClick={onClick}>追加</button>
+      <input
+        placeholder="TODOを入力"
+        value={todoText}
+        onChange={onChange}
+        disabled={disabled}
+      />
+      <button onClick={onClick} disabled={disabled}>
+        追加
+      </button>
     </div>
   );
 };
